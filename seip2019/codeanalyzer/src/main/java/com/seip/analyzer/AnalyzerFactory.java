ea6a897db7380a948bd2526cb7bb94de69d355d9
Christos-Michail Faltsetas <chrisfaltsetas@gmail.com>
2019-04-02 22:16:32 +0300

    Implement createAnalyzer method + documentation

--- seip2019/codeanalyzer/src/main/java/com/seip/analyzer/AnalyzerFactory.java
+++ seip2019/codeanalyzer/src/main/java/com/seip/analyzer/AnalyzerFactory.java
@@ -3,11 +3,25 @@ package com.seip.analyzer;
 /**
  * @author chrisfaltsetas
  * 
+ * This class follows the Factory pattern as the Factory
+ * and creates Analyzers depending on the type of the analyzer.
+ * StringAnalyzer for "string" type and RegexAnalyzer for "regex" type.
  */
 public class AnalyzerFactory {
-
+    
+    /**
+     * Creates and returns an Analyzer based on the given type.
+     * The default analyzer is //TODO
+     * @param type String
+     * @return
+     */
     public Analyzer createAnalyzer(String type) {
-        //TODO
-        return null;
+        Analyzer analyzer;
+        if (type.equals("string")) {
+            analyzer = new StringAnalyzer();
+        } else /*if (type.equals("regex"))*/ {
+            analyzer = new RegexAnalyzer();       
+        }
+        return analyzer;
     }
 }
