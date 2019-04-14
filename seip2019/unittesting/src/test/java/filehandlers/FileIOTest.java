5bad7340456454219850ac0d916fef904e38eade
Christos-Michail Faltsetas <chrisfaltsetas@gmail.com>
2019-04-14 03:20:57 +0300

    Add FileIO unit test cases + documentation

--- seip2019/unittesting/src/test/java/filehandlers/FileIOTest.java
+++ seip2019/unittesting/src/test/java/filehandlers/FileIOTest.java
@@ -1,48 +1,55 @@
 package filehandlers;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
 
-/***
- * A class that offers file handling operations.
- * It doesn't follow the "Utility" class strict definition 
- * that was introduced during the Design Patterns sessions.
- * @author agkortzis
+/**
+ * Provides test cases for the file reading method
+ * of the FileIO class.
+ * @author chrisfaltsetas
+ *
  */
 public class FileIOTest {
-	/***
-	 * This is a simple method that opens and reads a 
-	 * file line by line.
-	 * @param filepath required path to the file
-	 * @return a int[] that contains the content of the file
-	 * @throws IllegalArgumentException when the file does not exist in the given path
-	 * @throws NumberFormatException when the input number is in an invalid form
-	 */
-	public int[] readFile(String filepath)  {
-		List<Integer> linesList = new ArrayList<>();
-		
-		
-		try (BufferedReader reader = new BufferedReader(new FileReader(filepath))){
-			String line;
-			while((line = reader.readLine()) != null) {
-				linesList.add(Integer.parseInt(line));
-			}
-		} catch (FileNotFoundException e) {
-			throw new IllegalArgumentException("File " + filepath + " does not exist");
-		} catch (IOException e) { 
-			throw new NumberFormatException();
-		}
-		
-		int[] linesArray = new int[linesList.size()];
-		for (int index=0; index<linesList.size(); index++) {
-			linesArray[index] = linesList.get(index);
-		}
-			
-		return linesArray;
-	}
-
+    FileIO io = new FileIO();
+    
+    /**
+     * A test case for reading a file containing
+     * valid input numbers.
+     */
+    @Test
+    public void test_readFile_valid() {
+        String filepath = "src/test/resources/valid-numbers.txt";
+        
+        int[] expected = new int[9];
+        expected[0] = 46;
+        expected[1] = 32;
+        expected[2] = -11;
+        expected[3] = 34;
+        expected[4] = 5;
+        expected[5] = -55;
+        expected[6] = 0;
+        expected[7] = 13;
+        expected[8] = -11;
+        
+        Assert.assertArrayEquals(expected, io.readFile(filepath));
+    }
+    
+    /**
+     * A test case for the exception caused by
+     * reading a file containing invalid input numbers.
+     */
+    @Test (expected = NumberFormatException.class)
+    public void test_readFile_invalid() {
+        String filepath = "src/test/resources/invalid-numbers.txt";
+        io.readFile(filepath);
+    }
+    
+    /**
+     * A test case for the exception caused by trying
+     * to read a file that does not exist in the given filepath
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void test_readFile_notFound() {
+        io.readFile("this/is/not/a/valid/filepath");
+    }
 }
